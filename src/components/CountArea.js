497e2a5b7f0939178213c9635d6eef2b0979558b
m3hdi-rostami <m3hdi.rostami@gmail.com>
2022-02-17 00:06:52 +0330

    Refactor count area component

--- src/components/CountArea.js
+++ src/components/CountArea.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CountArea = ({ items }) => {
   return (
-    <div className="bg-slate-700 text-slate-200 font-bold w-2/3 mx-auto p-5 mb-3 rounded-lg">
+    <div className="bg-slate-700 text-slate-200 font-bold mx-auto p-5 mb-3 rounded-lg">
       <div className="flex align-center justify-between border-b-2 border-slate-300 pb-2 mb-2">
         <span className="flex align-center">All items:</span>
         <span className="bg-blue-500 py-1 px-3 rounded-full">
